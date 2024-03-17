07e10194d10742426bc4391184e500b30aa1786a
usedillusion <richard.downer81@gmail.com>
2024-03-17 16:51:30 +0000

    finished title animation

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,6 +1,10 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-
+import { gsap } from "gsap";
 /**
  * Props for `Hero`.
  */
@@ -10,6 +14,49 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const component = useRef(null);
+
+    useEffect(()=>{
+      let ctx = gsap.context(()=>{
+        const tl = gsap.timeline()
+
+          tl.fromTo(".name-animation",{
+            x: -500, opacity:0, rotate: -60, 
+          },
+          {
+            x:0,
+            opacity: 1,
+            rotate:0,
+            ease:"circ.out",
+            duration:0.75,
+            transformOrigin: "left top",
+            stagger:{
+              each: 0.1,
+              from: "random",
+            },
+          }
+          
+          );
+
+          tl.fromTo(".job-title", {
+            y:20,
+            opacity:0,
+            scale:1.2
+          }, {
+            opacity:1,
+            y:0,
+            duration:1,
+            scale:1,
+            ease:"elastic.out(2,0.3)",
+
+          });
+
+      }, component);
+      return () => ctx.revert();
+      }, []);
+    
+    
+
 
 const renderLetters = (name:KeyTextField, key:string) =>{
   if (!name) return;
@@ -24,6 +71,7 @@ const renderLetters = (name:KeyTextField, key:string) =>{
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
         <div className="col-start-1 md:row-start-1">
@@ -38,8 +86,8 @@ const renderLetters = (name:KeyTextField, key:string) =>{
         {renderLetters(slice.primary.last_name, "last")}
         </span>
           </h1>
-          <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text 
-          text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl">
+          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text 
+          text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
             {slice.primary.tag_line}
           </span>
       </div>

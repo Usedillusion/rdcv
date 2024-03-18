a6628b37d78d4fcbd8aaa8b6d9c0e2a0dfbea031
usedillusion <richard.downer81@gmail.com>
2024-03-18 18:45:45 +0000

    justify

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -8,7 +8,7 @@ import gsap from "gsap"
 
 export default function Shapes(){
     return(
-        <div className="row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0">
+        <div className="row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0 ">
             <Canvas className="z-0" shadows gl={{antialias:false}} dpr={[1, 1.5]} camera={{position:[0, 0, 25], fov:45, near:1, far:60}}>
             <Suspense fallback={null}>
                 <Geometries/>
